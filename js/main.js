1f2b6cafc2dc2d7a106adf1335753aaeb9ed8090
mcle0341 <mcle0341@algonquinlive.com>
2016-03-15 17:08:42 -0400

    added headers

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
-var $header = $('header');
+var $body = $('body');
+var $headerTwo = $('<h2>');
+var $ulTwo = $('<ul>');
+var $headerOne = $('<h2>');
+var $ulOne = $('<ul>');
+var $ul = $('<ul>')
+var $li = $('ul li');
 
-$header.append('Below ground veggies')
+$body.append($headerTwo);
+$headerTwo.html('Below Ground Veggies');
+$body.append($ulTwo);
+$body.append($headerOne);
+$headerOne.html('Above Ground Veggies');
+$body.append($ulOne);
+
+$li.each(function () {
+	if ($(this).hasClass('below')) {
+		$ulTwo.append($(this));
+	} else {
+		$ulOne.append($(this));
+	}
+});
